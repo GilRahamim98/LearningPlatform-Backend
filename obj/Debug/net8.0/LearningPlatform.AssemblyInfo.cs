b80577f1b184937968d646f8c6899de0abfa337d
GilRahamim98 <gilrahamimgr@gmail.com>
2025-03-03 19:21:23 +0200

    Change dtos

--- obj/Debug/net8.0/LearningPlatform.AssemblyInfo.cs
+++ obj/Debug/net8.0/LearningPlatform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cfd83ec586ff2c6e1a3e3aa20ae6473c0557340")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9dd5a1b23d66ad1c0ab595d65a506de422db37")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
